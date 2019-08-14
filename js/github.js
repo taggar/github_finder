accf3d7980116c09c2d4e5547d3f7559322bfcba
taggar <rvdeede@gmail.com>
2019-08-14 12:03:43 +0200

    WIP graphQL

--- js/github.js
+++ js/github.js
@@ -6,7 +6,13 @@ https://developer.github.com/v4/guides/forming-calls/
 
 const client_id = '538f75d2504eb830e987';
 const client_secret = 'e63150a06acdb1db5ef9c6a960ea9332b3db3eb6';
-const baseUrl = 'https://api.github.com/graphql';
+const personalToken = 'e4ff3e4cb181afb74edb6f8900afc5486983076c';
+const baseUrl = `https://api.github.com/graphql`;
+const headers = new Headers();
+headers.set('Content-type', 'application/json');
+headers.set('Authorization', `Bearer ${client_secret}`);
+headers.set('User-Agent', client_id);
+//headers.set('credentials', 'include');
 const query = `
 query {
   user(login: "taggar") {
@@ -39,14 +45,11 @@ query {
 const githubUserSearch = async event => {
   fetch(baseUrl, {
     method: 'POST',
-    headers: new Headers({
-      'Content-type': 'application/json',
-      Authorization: `Bearer ${client_secret}`
-    }),
+    headers: headers,
     body: JSON.stringify(query)
   }).then(result => {
     console.log(result);
-    showUserInfo(result);
+    //showUserInfo(result);
     //showUserRepo(result);
   });
 };
