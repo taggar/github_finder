dc9f4e0ec26fca0dd1885c1c76e419517f958e52
taggar <rvdeede@gmail.com>
2019-08-14 10:33:03 +0200

    WIP switch to graphql

--- js/github.js
+++ js/github.js
@@ -1,31 +1,52 @@
 // api calls
 
-const baseUrl = 'https://api.github.com/users/';
+/*
+https://developer.github.com/v4/guides/forming-calls/
+*/
+
 const client_id = '538f75d2504eb830e987';
 const client_secret = 'e63150a06acdb1db5ef9c6a960ea9332b3db3eb6';
+const baseUrl = 'https://api.github.com/graphql';
+const query = `
+query {
+  user(login: "taggar") {
+    login
+    url
+    avatarUrl
+    bio
+    company
+    email
+    followers {
+      totalCount
+    }
+    following {
+      totalCount
+    }
+    isHireable
+    repositories (first: 10, privacy: PUBLIC) {
+      totalCount
+      nodes {
+        name
+        homepageUrl
+        watchers {
+          totalCount
+        }
+      }
+    }
+  }
+}`;
 
-function githubUserSearch() {
-
-    doXhrGetJson(`${baseUrl}${event.target.value}?client_id=${client_id}&client_secret=${client_secret}`)
-        .then(function (result) {
-
-            showUserInfo(result);
-            doXhrGetJson(`${result.repos_url}?client_id=${client_id}&client_secret=${client_secret}`)
-                .then(function (result) { showUserRepo(result); })
-        });
-}
-
-/* Generic function to get an ajax json response
-===============================================*/
-function doXhrGetJson(url) {
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.onload = function () {
-            resolve(this.response);
-        };
-        xhr.onerror = reject;
-        xhr.responseType = 'json';
-        xhr.open('GET', url);
-        xhr.send();
-    });
-}
+const githubUserSearch = async event => {
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: new Headers({
+      'Content-type': 'application/json',
+      Authorization: `Bearer ${client_secret}`
+    }),
+    body: JSON.stringify(query)
+  }).then(result => {
+    console.log(result);
+    showUserInfo(result);
+    //showUserRepo(result);
+  });
+};
