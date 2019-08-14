7e08354957d34d2493cb5a4a9f8c93b83c525b68
taggar <rvdeede@gmail.com>
2019-08-14 12:03:43 +0200

    WIP graphQL

--- js/ui.js
+++ js/ui.js
@@ -4,39 +4,33 @@ function showUserInfo(user) {
   let target = document.getElementById('userInfo');
   target.innerHTML = `
     <div class="  card card-body mb-3">
-        <div class="row">
-            <div class="col-md-3">
-                <img class="img-fluid mb-2" src="${user.avatarUrl}">
-                    <a href="${
-                      user.url
-                    }" target="_blank" class="btn btn-primary btn-block">View&nbsp;Profile</a>
-      </div>
-                <div class="col-md-9">
-                    <span class="badge badge-primary">Repo public: ${
-                      user.repositories.totalCount
-                    }</span>
-                    <span class="badge badge-success">Followers: ${
-                      user.followers
-                    }</span>
-                    <span class="badge badge-info">Following: ${
-                      user.following
-                    }</span>
-                    <br><br>
-                        <ul class="list-group">
-                            <li class="list-group-item">Company:${
-                              user.company
-                            }</li>
-                            <li class="list-group-item">Website/Blog: <a href="${
-                              user.blog
-                            }">${user.blog} </a></li>
-                            <li class="list-group-item">Location: ${
-                              user.location
-                            }</li>
-                            <li class="list-group-item">Member Since: ${
-                              user.created_at
-                            }</li>
-                        </ul>
-                </div>
+      <div class="row">
+        <div class="col-md-3">
+            <img class="img-fluid mb-2" src="${user.avatarUrl}">
+                <a href="${
+                  user.url
+                }" target="_blank" class="btn btn-primary btn-block">View&nbsp;Profile</a>
+        </div>
+        <div class="col-md-9">
+            <span class="badge badge-primary">Repo public: ${
+              user.repositories.totalCount
+            }</span>
+            <span class="badge badge-success">Followers: ${
+              user.followers
+            }</span>
+            <span class="badge badge-info">Following: ${user.following}</span>
+            <br><br>
+                <ul class="list-group">
+                    <li class="list-group-item">Company:${user.company}</li>
+                    <li class="list-group-item">Website/Blog: <a href="${
+                      user.websiteUrl
+                    }">${user.websiteUrl} </a></li>
+                    <li class="list-group-item">Location: ${user.location}</li>
+                    <li class="list-group-item">Member Since: ${
+                      user.created_at
+                    }</li>
+                </ul>
+        </div>
       </div>
     </div>`;
 }
