ca6c92d07c1de4ce824d26eef13a29c47a009f29
taggar <rvdeede@gmail.com>
2019-08-14 15:54:57 +0200

    First working version with graphQL

--- js/github.js
+++ js/github.js
@@ -10,46 +10,31 @@ const personalToken = 'e4ff3e4cb181afb74edb6f8900afc5486983076c';
 const baseUrl = `https://api.github.com/graphql`;
 const headers = new Headers();
 headers.set('Content-type', 'application/json');
-headers.set('Authorization', `Bearer ${client_secret}`);
-headers.set('User-Agent', client_id);
+headers.set('Authorization', `bearer ${personalToken}`);
+headers.set('User-Agent', 'taggar');
 //headers.set('credentials', 'include');
-const query = `
-query {
-  user(login: "taggar") {
-    login
-    url
-    avatarUrl
-    bio
-    company
-    email
-    followers {
-      totalCount
-    }
-    following {
-      totalCount
-    }
-    isHireable
-    repositories (first: 10, privacy: PUBLIC) {
-      totalCount
-      nodes {
-        name
-        homepageUrl
-        watchers {
-          totalCount
-        }
-      }
-    }
-  }
-}`;
+let query = `query user($user:String!) { user(login: $user) { login createdAt url avatarUrl websiteUrl bio company location email followers { totalCount } following { totalCount } isHireable repositories (first: 10, privacy: PUBLIC) { totalCount nodes { name homepageUrl watchers { totalCount } forks {totalCount} stargazers {totalCount} }}}}`;
 
 const githubUserSearch = async event => {
-  fetch(baseUrl, {
+  let variables = {
+    user: event.target.value
+  };
+
+  let response = await fetch(baseUrl, {
     method: 'POST',
     headers: headers,
-    body: JSON.stringify(query)
-  }).then(result => {
-    console.log(result);
-    //showUserInfo(result);
-    //showUserRepo(result);
+    body: JSON.stringify({ query, variables })
   });
+
+  let result = await response.json();
+
+  showUserInfo(result.data);
+  showUserRepo(result.data);
+};
+
+const queryToGraphiQLQueryString = query => {
+  let lines = query.trim().split('\n');
+  let queryLines = lines.slice(1, -1).map(line => line.trim());
+
+  return encodeURIComponent(`{${queryLines.join(' ')}}`);
 };
